921bc4a875dfaecd0b82832e389ecceac79e7aa3
alex <alex@alexchiri.com>
2020-02-16 19:17:26 +0100

    Bump version to 0.1.5 Put back the tag grep for cross releases.

--- ci/install.sh
+++ ci/install.sh
@@ -33,7 +33,7 @@ main() {
     # This fetches latest stable release
     local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
                        | cut -d/ -f3 \
-                       | grep -E '^[0.1.0-9.]+$' \
+                       | grep -E '^v[0.1.0-9.]+$' \
                        | $sort --version-sort \
                        | tail -n1)
     curl -LSfs https://japaric.github.io/trust/install.sh | \
